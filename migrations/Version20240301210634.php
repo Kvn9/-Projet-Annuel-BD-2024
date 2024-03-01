75cf329d1784e60ec6c86f8bc6398c2cc26c7f7b
ahamie71 <ahmad.hamieh2002@hotmail.com>
2024-03-01 23:03:44 +0100

    Inscription et connection

---
+++ migrations/Version20240301210634.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240301210634 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE used_storage_space used_storage_space INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE used_storage_space used_storage_space INT NOT NULL');
+    }
+}
