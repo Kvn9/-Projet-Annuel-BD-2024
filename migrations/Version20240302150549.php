b91a3cbd1c6108f8165068cb10e771caca6f035b
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-05-30 22:03:08 +0200

    file

---
+++ migrations/Version20240302150549.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240302150549 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE file (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, file_id INT NOT NULL, file_name VARCHAR(255) NOT NULL, file_path VARCHAR(255) NOT NULL, file_size VARCHAR(255) NOT NULL, upload_date DATETIME NOT NULL, file_format VARCHAR(255) DEFAULT NULL, INDEX IDX_8C9F361079F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE invoice (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, invoiceid INT NOT NULL, invoice_date DATETIME NOT NULL, description_of_item_in_voiced VARCHAR(255) DEFAULT NULL, unit_price_tax INT NOT NULL, quantity INT NOT NULL, vatamount VARCHAR(255) DEFAULT NULL, total_amount_including_taxes INT DEFAULT NULL, INDEX IDX_9065174479F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE space_purchase (id INT AUTO_INCREMENT NOT NULL, id_user_id INT DEFAULT NULL, purchase_id INT NOT NULL, purchase_date DATETIME NOT NULL, purchased_space VARCHAR(255) NOT NULL, total_amount INT NOT NULL, INDEX IDX_19900C4279F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, postal_address VARCHAR(255) NOT NULL, registration_date DATETIME NOT NULL, used_storage_space INT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F361079F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_9065174479F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE space_purchase ADD CONSTRAINT FK_19900C4279F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE file DROP FOREIGN KEY FK_8C9F361079F37AE5');
+        $this->addSql('ALTER TABLE invoice DROP FOREIGN KEY FK_9065174479F37AE5');
+        $this->addSql('ALTER TABLE space_purchase DROP FOREIGN KEY FK_19900C4279F37AE5');
+        $this->addSql('DROP TABLE file');
+        $this->addSql('DROP TABLE invoice');
+        $this->addSql('DROP TABLE space_purchase');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
